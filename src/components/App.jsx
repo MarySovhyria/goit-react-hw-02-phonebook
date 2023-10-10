db1ef31128748f93e30b71cfcde0040b15dac562
MarySovhyria <111424962+MarySovhyria@users.noreply.github.com>
2023-10-10 21:44:46 +0100

    first commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,12 @@
-export const App = () => {
+import PhoneApp from "./phoneList/phone";
+
+function App() {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+    <PhoneApp></PhoneApp>
     </div>
+   
   );
-};
+}
+
+export default App;
