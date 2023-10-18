1b4a08caa80a4a873137c8fe1d8f7760db43dec6
MarySovhyria <111424962+MarySovhyria@users.noreply.github.com>
2023-10-18 13:51:12 +0100

    first commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,12 +5,7 @@ import Filter from './phoneBook/filter';
 
 class App extends Component {
   state = {
-    contacts: [
-      // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: JSON.parse(localStorage.getItem('contacts')),
     filter: '',
   };
 
@@ -51,6 +46,10 @@ class App extends Component {
       contacts: prevState.contacts.filter(({ id }) => id !== contactId),
     }));
   };
+
+  componentDidUpdate(prevProps, prevState) {
+    localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+  }
 
   render() {
     const { contacts, filter } = this.state;
